eb7241f61041347becd992b3dd07707990051921
Manoel Fernando C. Bentes <mfcbentes.stm@gmail.com>
2021-05-10 20:30:05 -0300

    Componente Header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from "./components/Header";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Header },
+};
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <header id="header">
+    <a>MfcbentesPost</a>
+    <button>Dashboard</button>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  components: {},
+};
+</script>
+
+<style scoped>
+#header {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 100%;
+  height: 60px;
+  background-color: #202225;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+}
+
+#header a {
+  text-decoration: none;
+  font-size: 30px;
+  color: #fff;
+  cursor: pointer;
+  font-weight: bold;
+}
+
+button {
+  cursor: pointer;
+  border: 0;
+  color: #fff;
+  background: transparent;
+  padding: 5px 15px;
+  outline: none;
+  font-weight: bold;
+}
+</style>
