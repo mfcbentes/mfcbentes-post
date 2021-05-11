7e76b23dfbd2916c68b4a103a3f9b545eb0dba38
Manoel Fernando C. Bentes <mfcbentes.stm@gmail.com>
2021-05-10 22:43:55 -0300

    Criar pagina Login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,15 +1,99 @@
 <template>
   <div id="login">
-    <h1>Página de Login</h1>
+    <div class="loginArea" v-if="login">
+      <h1>Entrar</h1>
+      <form>
+        <input type="email" placeholder="mail@email.com" v-model="email" />
+        <input type="password" placeholder="Sua senha..." v-model="senha" />
+        <button type="submit">Acessar</button>
+      </form>
+      <a v-on:click="toogleBtn">Criar uma conta</a>
+    </div>
+    <div class="loginArea" v-else>
+      <h1>Cadastrar</h1>
+      <form>
+        <input type="text" placeholder="Nome" v-model="nome" />
+        <input type="email" placeholder="mail@email.com" v-model="email" />
+        <input type="password" placeholder="Sua senha..." v-model="senha" />
+        <button type="submit">Cadastrar</button>
+      </form>
+      <a v-on:click="toogleBtn">Já possuo uma conta</a>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Login",
+  data() {
+    return {
+      nome: "",
+      email: "",
+      senha: "",
+      login: true,
+    };
+  },
+  methods: {
+    toogleBtn() {
+      this.login = !this.login;
+      this.nome = "";
+      this.email = "";
+      this.senha = "";
+    },
+  },
   components: {},
 };
 </script>
 
 <style scoped>
+h1 {
+  text-align: center;
+  color: #fff;
+}
+
+.loginArea {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  padding: 20px;
+  margin-top: 25px;
+  max-width: 600px;
+  background: #4c5059;
+  border-radius: 5px;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  margin: 0 25px;
+}
+
+input {
+  margin-bottom: 10px;
+  height: 15px;
+  width: 300px;
+  font-size: 18px;
+  padding: 10px;
+  outline: none;
+  border: 0;
+  background: rgb(241, 241, 241);
+}
+
+button {
+  height: 35px;
+  border: 0;
+  background-color: #7289da;
+  color: #fff;
+  font-size: 16px;
+  font-weight: 600;
+  outline: none;
+  cursor: pointer;
+}
+
+.loginArea a {
+  margin-top: 20px;
+  text-decoration: none;
+  color: #fff;
+  cursor: pointer;
+}
 </style>
