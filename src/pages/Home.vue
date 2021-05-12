97506ba7a20cff9cb4d28ecd21006174ad88ce6d
Manoel Fernando C. Bentes <mfcbentes.stm@gmail.com>
2021-05-11 22:07:46 -0300

    Criando a pagina Home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,29 @@
 <template>
   <div id="home">
-    <h1>Página Home</h1>
+    <div class="newpost">
+      <h2>Bem vindo de volta!</h2>
+      <span>Compartilhe seu dia</span>
+      <textarea placeholder="O que está fazendo hoje?" rows="15"> </textarea>
+      <button>Compartilhar</button>
+    </div>
+    <div class="postarea">
+      <article class="post">
+        <h1>Manoel</h1>
+        <p>Olá, bora codar???</p>
+        <div class="action-post">
+          <button>2 curtida(s)</button>
+          <button>Veja o post completo</button>
+        </div>
+      </article>
+      <article class="post">
+        <h1>Fernando</h1>
+        <p>Olá, é o meu primeiro post.</p>
+        <div class="action-post">
+          <button>1 curtida(s)</button>
+          <button>Veja o post completo</button>
+        </div>
+      </article>
+    </div>
   </div>
 </template>
 
@@ -12,4 +35,5 @@ export default {
 </script>
 
 <style scoped>
+@import "../css/home.css";
 </style>
