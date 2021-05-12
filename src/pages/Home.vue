a13c040e424a23f628ee35c7749893b500898a3a
Manoel Fernando C. Bentes <mfcbentes.stm@gmail.com>
2021-05-11 22:29:51 -0300

    Criar funcao de cadastro de posts

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,8 +3,13 @@
     <div class="newpost">
       <h2>Bem vindo de volta!</h2>
       <span>Compartilhe seu dia</span>
-      <textarea placeholder="O que está fazendo hoje?" rows="15"> </textarea>
-      <button>Compartilhar</button>
+      <textarea
+        placeholder="O que está fazendo hoje?"
+        rows="15"
+        v-model="input"
+      >
+      </textarea>
+      <button v-on:click="createPost">Compartilhar</button>
     </div>
     <div class="postarea">
       <article class="post">
@@ -28,9 +33,45 @@
 </template>
 
 <script>
+import firebase from "../services/firebaseConnection";
+
 export default {
   name: "Home",
-  components: {},
+  data() {
+    return {
+      input: "",
+      user: {},
+    };
+  },
+  created() {
+    const user = localStorage.getItem("devPost");
+    this.user = JSON.parse(user);
+  },
+  methods: {
+    async createPost() {
+      if (this.input === "") {
+        return;
+      }
+
+      await firebase
+        .firestore()
+        .collection("posts")
+        .add({
+          created: new Date(),
+          content: this.input,
+          autor: this.user.nome,
+          userId: this.user.uid,
+          likes: 0,
+        })
+        .then(() => {
+          this.input = "";
+          console.log("Post salvo com sucesso!");
+        })
+        .catch((erro) => {
+          console.log("Erro ao criar o post: " + erro);
+        });
+    },
+  },
 };
 </script>
 
